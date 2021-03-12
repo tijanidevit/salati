29a1b67eaa2f8b3d82ccac5c4d75620689ccd051
Hamza Mounir <hamza.pixelle@gmail.com>
2021-03-12 16:26:04 +0100

    [SALATI] fix: binding

--- config/default.js
+++ config/default.js
@@ -12,6 +12,6 @@ module.exports = {
   defaultTimezone: 'Europe/Brussels',
   port: process.env.PORT || 3000,
   secret: process.env.SECRET,
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || '0.0.0.0',
   mongo: process.env.MONGO || 'mongodb://localhost:27017/salati',
 };
